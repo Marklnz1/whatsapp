cd0dd3258e8a0934372027cb2ce72f7d34a2759e
Marklnz1 <jhon4lnz@gmail.com>
2024-10-27 20:53:55 -0500

    aasdasd

--- controller/whatsAppController.js
+++ controller/whatsAppController.js
@@ -81,7 +81,7 @@ module.exports.receiveMessage = async (req, res) => {
       metaFileName = mediaData.filename;
       metadata = await saveMedia(mediaId, typeMessage);
 
-      console.log("SE OBTUVO EL MEDIANAME " + metadata);
+      console.log("SE OBTUVO EL MEDIANAME " + util.inspect(metadata, true, 99));
     }
 
     //===============================================================
