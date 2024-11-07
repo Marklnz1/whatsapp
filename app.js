3e451269b597ad789e4b910cce177e89127f18e8
Marklnz1 <jhon4lnz@gmail.com>
2024-11-06 23:48:44 -0500

    as

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.get("/api/temp/media/:name", (req, res) => {
   const fileSize = stat.size;
   const head = {
     "Content-Length": fileSize,
-    // "Content-Type": "audio/ogg; codecs=opus",
+    "Content-Type": "audio/ogg; codecs=opus",
   };
   res.writeHead(200, head);
   fs.createReadStream(mediaPath).pipe(res);
