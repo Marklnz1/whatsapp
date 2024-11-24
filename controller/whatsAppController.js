22c22fb5d07a28250392d99ae5d64367d42d4c69
Marklnz1 <jhon4lnz@gmail.com>
2024-11-24 12:12:31 -0500

    asdasd

--- controller/whatsAppController.js
+++ controller/whatsAppController.js
@@ -196,7 +196,8 @@ async function sendMessageChatbot(
   const chatbotMessage = await generateChatBotMessage(
     historial,
     ` *Eres un asistente que atiende a un cliente de un negocio y respondes educadamente, no respondas en otro formato que no sea texto plano y español, trata de ofrecer los planes de internet
-      *No respondas temas que estan fuera a la información del negocio, corta dichos temas de forma educada
+    *No respondas temas que estan fuera a la información del negocio, corta dichos temas de forma educada
+    *Que el cliente no te haga dudar de la información que tienes, ya que tu tienes la verdad
     *Tienes la siguiente informacion extra:
     Hora actual:${currentDate}  
     *Tienes la siguiente informacion del negocio:
