fe7c389d0e2b01613db299f14c1f7dd8e5cd832f
Marklnz1 <jhon4lnz@gmail.com>
2024-12-08 04:03:30 -0500

    asdasd

--- app.js
+++ app.js
@@ -23,7 +23,11 @@ const mapLinkTemp = new Map();
 const path = require("path");
 const fs = require("fs");
 
-const io = new Server(server);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//   },
+// });
 app.use(express.json({ limit: "50mb" }));
 // app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // const cors = require("cors");
