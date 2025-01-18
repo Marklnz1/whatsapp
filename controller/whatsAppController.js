e8e57e5dc5c5842ae5a95ee0bd248aefd69fc58f
Marklnz1 <jhon4lnz@gmail.com>
2025-01-17 20:35:09 -0500

    aaa

--- controller/whatsAppController.js
+++ controller/whatsAppController.js
@@ -222,10 +222,10 @@ async function generateChatBotMessage(
   return chatCompletion.choices[0].message.content;
 }
 function extractNumberAndContent(input) {
-  const regex = /^\s*\[([^]]*?(-?\d+)[^]]*?)?]\s*/;
+  const regex = /^\s*\[([^]]*?(\d+)[^]]*?)]\s*/;
   const match = input.match(regex);
   if (match) {
-    const numberMatch = match[1] ? match[1].match(/-?\d+/) : null;
+    const numberMatch = match[1] ? match[1].match(/\d+/) : null;
     const number = numberMatch ? parseInt(numberMatch[0]) : -1;
     const content = input.replace(regex, "").trim();
     return { number: number, content: content };
