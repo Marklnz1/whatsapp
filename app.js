4ec445a99ef0c9bc379625992dda075ee583c01c
Marklnz1 <jhon4lnz@gmail.com>
2025-02-04 19:36:20 -0500

    aa

--- app.js
+++ app.js
@@ -240,8 +240,8 @@ SyncServer.syncPost({ model: MessageStatus, tableName: "messageStatus" });
 SyncServer.syncPost({
   model: Message,
   tableName: "message",
-  onInsertPrevious: ({ docs }) => {
-    for (const doc of docs) {
+  onInsertPrevious: ({ insertableDocs }) => {
+    for (const doc of insertableDocs) {
       console.log(
         "CUMPLE??",
         doc["sentStatus"] == "not_sent" && doc["sent"] == "true"
